e2e2df2ba7e36e5676e96369d44d8ec41f905374
Prashanth-500apps <prashanth.uppada@500apps.com>
2022-05-04 19:06:11 -0400

    4-5-22

--- src/components/Assignment(28-4-22)/Q3.vue
+++ src/components/Assignment(28-4-22)/Q3.vue
@@ -1,31 +1,32 @@
 <template>
     <div>
-        <b-button type="submit" @click="getData" >GetData</b-button>
-        <b-table striped hover :items="items"></b-table>
-        {{items}}
+         <b-button type="submit" @click="fun" >GetData</b-button>
+       <!-- <b-table striped hover :items="items"></b-table> -->
     </div>
 </template>
 
 <script>
-import axios from "axios"
+//import axios from "axios"
 export default {
     name:"QuesTion3",
     data(){
         return{
-          items:[],
-          fin:"",
+        //   items:[],
+        //   fin:"",
         }
     },
+    // methods:{
+    //     async getData() {
+    //        let response = await axios.get("https://www.templatemonster.com/free-html-website-templates/");
+    //        let responseText = await response.data;
+    //        this.items = JSON.stringify(responseText)
+    //        }   
+    // },
     methods:{
-        async getData() {
-           let response = await axios.get("https://www.templatemonster.com/free-html-website-templates/");
-           let responseText = await response.data;
-           this.items = JSON.stringify(responseText)
-           }   
-    },
-    mounted() {
-     this.fin = document.getElementsByTagName("a")
-     console.log(this.fin)
+          fun(){
+        const ans = document.getElementsByTagName('*');
+        console.log(ans)
+          }
     }
 }
 </script>
