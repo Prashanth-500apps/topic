8f08b9ea3184662c43c55e4c18b0dbfc9e2e4b60
Prashanth-500apps <prashanth.uppada@500apps.com>
2022-05-04 19:09:51 -0400

    4-5-22

--- src/components/Assignment11(26-4-22)/Que3.vue
+++ src/components/Assignment11(26-4-22)/Que3.vue
@@ -1,14 +1,14 @@
 <template>
     <b-row><b-col md="8"><div align="center">
   <b-card>
-    <b-form id="form"> Enter First Name:<b-form-input  id="sample1" v-model="value1" placeholder="enter Firstname"></b-form-input>
-      Enter LastName:<b-form-input id="sample2" v-model="value2" placeholder="enter Lastname"></b-form-input>
-      <b-button @click="Add()">Add</b-button>
-      <b-button @click="Delete()">Delete</b-button>
-      <b-button @click="Update()">Update</b-button>
+    <b-form id="form"> Enter First Name:<b-form-input v-model="value1" placeholder="enter Firstname"></b-form-input>
+      Enter LastName:<b-form-input  v-model="value2" placeholder="enter Lastname"></b-form-input>
+      <b-button variant="success" @click="Add()">Add</b-button>
+      <b-button  variant="danger" @click="Delete()">Delete</b-button>
+      <b-button variant="primary" @click="Update()">Update</b-button>
     </b-form>
-    <b-table striped hover :items="items" :fields="fields">
-    </b-table>
+     <b-table striped hover :items="items" :fields="fields">  
+    </b-table> 
     </b-card>
   </div></b-col></b-row>
 </template>
@@ -16,16 +16,16 @@
 <script>
 export default {
   name:"QuE3",
-  data() {
-    return {
-      items: [{ firstname: "", lastname: "" }],
+  data(){
+    return{
+      items: [{ firstname: "", lastname: ""}],
       fields: ["firstname", "lastname"],
       value1:"",
       value2:"",
     };
   },
-  methods: {
-    Add() {
+  methods:{
+    Add(){
       this.items.push({
         firstname: this.value1,
         lastname: this.value2,
@@ -33,11 +33,14 @@ export default {
       this.value1="",
       this.value2=""
     },
-    Delete() {
+    Delete(){
       this.items.pop();
     },
     Update(){
-     
+       this.items.put({
+         firstname:this.value1,
+         lastname:this.value2,
+       });
     }
   },
 };
