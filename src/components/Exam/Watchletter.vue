2bd65dbb8c8abbf8d83a899d933d6e87b75dde1d
Prashanth-500apps <prashanth.uppada@500apps.com>
2022-05-10 19:52:22 -0400

    exam

---
+++ src/components/Exam/Watchletter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <b-row><b-col md="8">
+    <b-form id="form">
+      Enter Text:<b-form-input id="name" v-model="lower" placeholder="Enter text"></b-form-input>
+    </b-form>
+    <b-form>To UpperCase
+      <b-form-input id="name" v-model="upper" placeholder="Enter product name"></b-form-input>
+    </b-form>
+    </b-col></b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestioN2",
+  data() {
+    return {
+      lower: "",
+      upper: "",
+    };
+  },
+  watch: {
+    lower: function () {
+      this.upper = this.lower.toUpperCase();
+    },
+  },
+};
+</script>
