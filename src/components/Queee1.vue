5d0625985a4296f6f922b2c4b770409355574552
Prashanth-500apps <102018682+Prashanth-500apps@users.noreply.github.com>
2022-05-10 08:10:24 -0400

    Add files via upload

---
+++ src/components/Queee1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-card>
+    <b-form id="form">
+      Enter First Name:<b-form-input  id="sample1" placeholder="enter First name"></b-form-input>
+      Enter LastName:<b-form-input id="sample2" placeholder="enter Last name"></b-form-input>
+      <b-button @click="Add()">Add</b-button>
+    </b-form>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"QuestION1",
+  data() {
+    return {
+      items: [{ firstname: "", lastname: "" }],
+      fields: ["firstname", "lastname"],
+    };
+  },
+  methods: {
+
+    Add() {
+      this.items.push({
+        firstname: document.getElementById("sample1").value,
+        lastname: document.getElementById("sample2").value,
+      });
+    },
+   
+  },
+};
+</script>
